ef9e38b54a15a2c32bb36e0eeb312833a9391fcc
safal312 <safal.bp2@gmail.com>
2021-07-01 22:08:46 +0400

    finalize

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// waiting until the document has loaded
 document.addEventListener('DOMContentLoaded', init);
 
 let introVideo, finalVideo, popup, loading;
@@ -7,8 +8,8 @@ let answer = {
     spicy: 0
 }
 
+// two functions for the loading screen
 function startLoading() {
-    // console.log("loading...");
     loading.style.opacity = 1;
     loading.style.pointerEvents = "all";
 }
@@ -22,9 +23,11 @@ function init() {
     let title = document.querySelector('.main-title');
     loading = document.querySelector('.loading');
     introVideo = document.querySelector('.intro-video video');
+
+    // removing the intro screen on click, starting loading screen
+    // loading the first video
+    // stopping loading screen
     title.addEventListener('click', () => {
-        let elem = document.documentElement;
-        // elem.requestFullscreen();
         title.style.opacity = 0;
         startLoading();
         setTimeout(() => {
@@ -34,15 +37,14 @@ function init() {
         }, 1000);
         introVideo.src = "videos/videos/First clip.mp4";
         introVideo.addEventListener("loadeddata", () => {
-            // console.log("here");
             stopLoading();
-            // introVideo.play();
             introVideo.addEventListener("ended", introVideoEnd);
         })
     })
 }
 
 function introVideoEnd() {
+    // showing the popup when the intro video ends
     popup = document.querySelector(".popup");
     let form = document.querySelector(".popup .container .spiciness");
 
@@ -56,18 +58,17 @@ function submitHandler(e) {
     startLoading();
     loading.innerHTML = "<img class='bg' src='images/bg.png'>Loading Story..";
     e.preventDefault();
-    // console.log("after submitt");
-    
 
     popup.style.pointerEvents = "none";
+
+    // getting data from the form
     answer.step = document.querySelector('input[name="steps"]:checked').value;
     answer.spicy = document.querySelector('input[name="spicy"]:checked').value;
-    console.log(answer);
     finalVideo = document.querySelector(".final-video video");
-    // finalVideo.load();
     
     introVideo.remove();
 
+    // selecting proper video depending on the answer
     if (answer.step == "Rice, Egg, Vegetables") {
         if (answer.spicy == "hot") {
             finalVideo.src = "videos/videos/rice, egg, spicy.mp4";
@@ -90,17 +91,11 @@ function submitHandler(e) {
     
     document.querySelector(".final-video").style.display = "block";
     popup.style.opacity = 0;
-
-    // setTimeout(() => {
-    //     popup.remove();
-    // }, 500);
     
     finalVideo.play();
+    // stoping the loading when final video has finished loading
     finalVideo.addEventListener("loadeddata", () => {
-        // console.log("stataghsgh");
-            stopLoading();    
-        
-        // finalVideo.play();
+        stopLoading();    
 
         finalVideo.addEventListener("ended", theEnd);
     })
@@ -108,13 +103,11 @@ function submitHandler(e) {
 }
 
 function theEnd() {
-    // console.log("heree");
+    // playing the ending audio at the end
     let bg = new Audio("audio/music.mp3");
     bg.volume = 0.5;
-    bg.loop = true;
     bg.play();
     let ending = document.querySelector(".the-end");
     ending.style.opacity = 1;
     ending.style.pointerEvents = "all";
-    // document.exitFullscreen();
 }
